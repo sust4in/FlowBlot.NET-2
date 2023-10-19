d8686bdb37161edd4a4f414cf54eaa849d4605c0
Julian Thome <jthome@gitlab.com>
2023-10-19 10:13:36 +0200

    minor

---
+++ src/Flows/Field-Sensitivity/Flow_Multi.cs
@@ -0,0 +1,22 @@
+namespace FlowBlot.Flows.Field_Sensitivity
+{
+    public class TestObject
+    {
+        public string Label { get; set; }
+    }
+    
+    public class Flow_Multi
+    {
+        public void Foo()
+        {
+            var to = new TestObject();
+            to.Label = FlowBlot.Model.Framework.Source();
+            Bar(to);
+        }
+        
+        public void Bar(TestObject to)
+        {
+            FlowBlot.Model.Framework.Sink(to.Label);
+        }
+    }
+}
