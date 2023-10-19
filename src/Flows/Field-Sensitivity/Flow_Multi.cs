3eade97faffdd4212b05442926e470862335ad29
testsastanalysis <testsastanalysis@proton.me>
2023-10-19 16:24:31 +0200

    test

--- src/Flows/Field-Sensitivity/Flow_Multi.cs
+++ src/Flows/Field-Sensitivity/Flow_Multi.cs
@@ -14,6 +14,13 @@ namespace FlowBlot.Flows.Field_Sensitivity
             Bar(to);
         }
         
+        public void Run2()
+        {
+            var to = new TestObject();
+            to.Label = FlowBlot.Model.Framework.Source();
+            Bar(to);
+        }
+        
         public void Bar(TestObject to)
         {
             FlowBlot.Model.Framework.Sink(to.Label);
