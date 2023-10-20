d1e99aada55c356796233f70a1968174b128ecc4
testsastanalysis <testsastanalysis@proton.me>
2023-10-20 14:31:48 +0200

    type

--- src/Flows/Xamples/SqlInjection.cs
+++ src/Flows/Xamples/SqlInjection.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data.SqlClient;
+using System.Numerics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlowBlot.Flows.Xamlpes
@@ -12,8 +13,10 @@ namespace FlowBlot.Flows.Xamlpes
             using (SqlConnection connection = new SqlConnection(""))
             {
                 connection.Open();
+                // implicit sanitization
+                var result = Int32.Parse(keyword);
                 // Create a SqlCommand object and specify the SQL query that you want to execute.
-                var sqlQuery = $"SELECT * FROM  Customers WHERE {keyword}";
+                var sqlQuery = $"SELECT * FROM  Customers WHERE income > {result}";
                 var command = new SqlCommand(sqlQuery, connection);
             }
             return null;
